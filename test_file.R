fbec3d4c98ac656e7597257963d8c9cea0918b69
Kaitlin Ryan <kryan23@nd.edu>
2021-08-18 15:11:14 -0400

    commit

--- test_file.R
+++ test_file.R
@@ -1,2 +1,26 @@
 x <- 1:10
 y <- 1:10
+plot(mtcars$disp, mtcars$mpg)
+abline(lm(mpg ~ disp, data = mtcars))
+install.packages(c("car", "ggplot2"))
+
+car::scatterplot(mpg ~ disp, 
+                 data = mtcars)
+library(ggplot2)
+ggplot(data = mtcars, 
+       mapping = aes(x = disp, y = mpg)) +
+  geom_point() +
+  geom_smooth(method = "lm") + 
+  theme_minimal()
+
+install.packages('plotly')
+
+#plotly::ggplotly(my_plot)
+
+st_joe_land <- read.delim(file = "stJoeLand.csv", sep = ",")
+
+install.packages(c("readr", "data.table"))
+
+st_joe_land_readr <- readr::read_csv(file = "stJoeLand.csv")
+
+st_joe_land_datatable <- data.table::fread(input = "stJoeLand.csv")
